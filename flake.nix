8cae2662c7c382abe6ee56f47aa621d5015df3a3
maxyw <maxy.ww@pm.me>
2025-12-02 18:45:13 -0800

    provide actual package "uwu-colors", with the old name under default for compat
    packages.x86_64-linux.default -> uwu_colors
    packages.x86_64-linux.uwu-colors -> uwu-colors
    
    hopefully this isn't too confusing, this is moreso something I just
    wanted to add to my own fork. it doesn't need to be upstreamed but the
    commit is here in case that's desired

--- flake.nix
+++ flake.nix
@@ -28,7 +28,8 @@
           clippy,
         }:
         buildRustPackage {
-          name = "uwu_colors";
+          pname = "uwu-colors";
+          version = "0.4.0";
 
           nativeBuildInputs = [
             clippy
@@ -38,22 +39,38 @@
           src = lib.cleanSource ./.;
         }
       ) { inherit (pkgs.rustPlatform) buildRustPackage; };
+
+      flakePackageOld = pkgs: (flakePackage pkgs).overrideAttrs {
+        pname = "uwu_colors";
+        postPatch = ''
+          substituteInPlace ./Cargo.toml \
+            --replace-fail "uwu-colors" "uwu_colors"
+        '';
+      };
     in
     {
       overlays = {
         default = final: prev: {
+          uwu-colors = flakePackageOld prev;
+        };
+        uwu-colors = final: prev: {
           uwu-colors = flakePackage prev;
         };
       };
 
       packages = forAllSystems (pkgs: {
-        default = flakePackage pkgs;
+        default = flakePackageOld pkgs;
+        uwu-colors = flakePackage pkgs;
       });
 
       apps = forAllSystems (pkgs: {
         default = {
           type = "app";
-          program = nixpkgs.lib.getExe' (flakePackage pkgs) "uwu_colors";
+          program = nixpkgs.lib.getExe' (flakePackageOld pkgs) "uwu_colors";
+        };
+        uwu-colors = {
+          type = "app";
+          program = nixpkgs.lib.getExe' (flakePackage pkgs) "uwu-colors";
         };
       });
     };
